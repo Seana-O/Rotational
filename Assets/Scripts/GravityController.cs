139fe00aa5c3fb1f2ee273f8a154563db302a58d
Seana <73309041+Seana-O@users.noreply.github.com>
2024-03-04 13:13:11 +0100

    added collision check

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -4,43 +4,66 @@ using UnityEngine;
 
 public class GravityController : MonoBehaviour
 {
-    public List<Rigidbody2D> rigidbodies = new List<Rigidbody2D>();
+    public List<Object> objects = new List<Object>();
+    public Vector2 gravityDirection = Vector2.zero;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-            SetGravity(GravityDirection.Up);
+            TrySetGravity(GravityDirection.Up);
         else if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-            SetGravity(GravityDirection.Down);
+            TrySetGravity(GravityDirection.Down);
         else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-            SetGravity(GravityDirection.Left);
+            TrySetGravity(GravityDirection.Left);
         else if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-            SetGravity(GravityDirection.Right);
+            TrySetGravity(GravityDirection.Right);
+
+        Physics2D.gravity = gravityDirection;
+    }
+
+    void TrySetGravity(GravityDirection dir)
+    {
+        foreach(Object obj in objects)
+            if(obj.moving) return;
+
+        SetGravity(dir);
     }
 
     void SetGravity(GravityDirection dir)
     {
         int gravityMultiplier = 250;
+        bool horizontalGravity = false;
 
         switch (dir)
         {
             case GravityDirection.Up:
-                Physics2D.gravity = new Vector2(0, gravityMultiplier);
+                gravityDirection = new Vector2(0, gravityMultiplier);
                 break;
             case GravityDirection.Down:
-                Physics2D.gravity = new Vector2(0, -gravityMultiplier);
+                gravityDirection = new Vector2(0, -gravityMultiplier);
                 break;
             case GravityDirection.Left:
-                Physics2D.gravity = new Vector2(-gravityMultiplier, 0);
+                gravityDirection = new Vector2(-gravityMultiplier, 0);
+                horizontalGravity = true;
                 break;
             case GravityDirection.Right:
-                Physics2D.gravity = new Vector2(gravityMultiplier, 0);
+                gravityDirection = new Vector2(gravityMultiplier, 0);
+                horizontalGravity = true;
                 break;
         }
+        
+        Physics2D.gravity = gravityDirection;
 
-        foreach(Rigidbody2D rb in rigidbodies)
-            rb.velocity = Vector2.zero;
+        foreach(Object obj in objects)
+        {
+            obj.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            obj.moving = true;
 
+            if(horizontalGravity)
+                obj.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionX;
+            else
+                obj.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+        }
     }
 
     enum GravityDirection
--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -1,11 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Object : MonoBehaviour
 {
+    GravityController gravityController;
+    public bool moving = false;
+
     private void Start()
     {
-        FindObjectOfType<GravityController>().rigidbodies.Add(GetComponent<Rigidbody2D>());
+        gravityController = FindObjectOfType<GravityController>();
+        gravityController.objects.Add(this);
+    }
+
+    private void Update()
+    {
+        if (moving)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+            GetComponent<Image>().raycastTarget = false;
+            Vector2 gravityDir = gravityController.gravityDirection.normalized;
+            //Vector2 rayCastPos = transformPos + (gameObject.GetComponent<RectTransform>().sizeDelta.x/2) * gravityDir;
+            Debug.Log(transform.position);
+            Debug.Log(transform.TransformPoint(transform.localPosition));
+            Vector3[] v = new Vector3[4];
+            GetComponent<RectTransform>().GetWorldCorners(v);
+
+            float size = v[2].x - v[0].x; // width and height should be the same
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, gravityDir, size/2 + 1);
+
+            Debug.Log(" ");
+            Debug.Log(v[0]);
+            Debug.Log(v[1]);
+            Debug.Log(v[2]);
+            Debug.Log(v[3]);
+            if(hit.collider != null /*&& (hit.collider.CompareTag("ClosedTile") || hit.collider.CompareTag("Box") || hit.collider.CompareTag("Player")*/)
+            {
+                moving = false;
+                gameObject.GetComponent<Rigidbody2D>().constraints &= RigidbodyConstraints2D.FreezePosition;
+            }
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            GetComponent<Image>().raycastTarget = true;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+        GetComponent<Image>().raycastTarget = false;
+        Vector2 gravityDir = gravityController.gravityDirection.normalized;
+        //Vector2 rayCastPos = transformPos + (gameObject.GetComponent<RectTransform>().sizeDelta.x/2) * gravityDir;
+        Debug.Log(transform.position);
+        Debug.Log(transform.TransformPoint(transform.localPosition));
+        Vector3[] v = new Vector3[4];
+        GetComponent<RectTransform>().GetWorldCorners(v);
+
+        float size = v[2].x - v[0].x; // width and height should be the same
+
+        Gizmos.DrawRay(transform.position, gravityDir * (size/2+1));
     }
 }
