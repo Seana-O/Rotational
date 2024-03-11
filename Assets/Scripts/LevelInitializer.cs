fbe2d6d3b91b29602974b3fd418016505841c948
Seana <73309041+Seana-O@users.noreply.github.com>
2024-03-11 12:12:37 +0100

    fixed rotation bug

--- Assets/Scripts/LevelInitializer.cs
+++ Assets/Scripts/LevelInitializer.cs
@@ -218,8 +218,8 @@ public class LevelInitializer : MonoBehaviour
 
     Vector2 GetWorldLocation(int gridX, int gridY)
     {
-        float width = setup.Width/2;
-        float height = setup.Height/2;
+        float width = (float)setup.Width/2;
+        float height = (float)setup.Height/2;
 
         float x = (gridX - width) * tileSize + tileSize/2;
         float y = -((gridY - height) * tileSize + tileSize/2);
